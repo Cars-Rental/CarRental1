9d3426527a73151238dee06df3ce5807a1a26825
mohamed080 <mohayman080@gmail.com>
2026-06-28 12:31:07 +0300

    return to default config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,21 +1,3 @@
-// import type { NextConfig } from "next";
-// import createNextIntlPlugin from "next-intl/plugin";
-
-// const nextConfig: NextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "res.cloudinary.com",
-//         pathname: "**",
-//       },
-//     ],
-//   },
-// };
-
-// const withNextIntl = createNextIntlPlugin();
-// export default withNextIntl(nextConfig);
-
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
@@ -28,14 +10,6 @@ const nextConfig: NextConfig = {
         pathname: "**",
       },
     ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:3000/:path*",
-      },
-    ];
   },
 };
 
